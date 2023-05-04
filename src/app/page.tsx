1a93f00c280cf5765d6c24d988317b6a747b06ea
Benny (YC0SPU) <chandra@benny.id>
2023-05-04 18:26:16 +0700

    Change layout for small screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,16 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-csjs-building bg-no-repeat bg-cover bg-center">
       <div className='main-container'>
-        <div className='flex'>
+        <div className='flex flex-col md:flex-row items-center'>
           <Image src={ LogoJaksel } 
-            className='w-16 h-16 md:w-28 md:h-28'
+            className='w-20 h-20 md:w-28 md:h-28'
             alt="Logo ORARI Jakarta Selatan" 
-            sizes="(max-width: 768px) 10vw,
-            (max-width: 1200px) 20vw,
-            30vw"
+            sizes="(max-width: 768px) 40vw,
+            50vw"
           />
-          <div className="mx-4 md:mx-8">
-            <p className='text-slate-100 text-lg md:text-4xl'>Organisasi Amatir Radio Indonesia</p>
-            <p className='text-slate-100 text-3xl md:text-6xl'>Lokal Jakarta Selatan</p>
+          <div className="mx-4 md:mx-8 mt-4 md:mt-0">
+            <p className='text-slate-100 text-center md:text-left text-slate-100 text-xs md:text-4xl'>Organisasi Amatir Radio Indonesia</p>
+            <p className='text-slate-100 text-center md:text-left text-slate-100 text-xl md:text-6xl'>Lokal Jakarta Selatan</p>
           </div>
         </div>
       </div>
